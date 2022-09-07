4af54b83f2859cb37e399080346853f775f22df8
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-07 23:22:38 +0530

    Line Space and delimiters in string

--- test_String_sum.py
+++ test_String_sum.py
@@ -16,6 +16,10 @@ class TestString_sum(unittest.TestCase):
     def test_alphabet(self):
         self.assertEqual(String_sum.StrSum("1,2,a,c"),7)
         self.assertEqual(String_sum.StrSum("1,2,a,c,d"),11) 
+    def test_delimiter(self):
+        self.assertEqual(String_sum.StrSum("//;\n1;2,"),3) 
+    def test_line_space(self):
+        self.assertEqual(String_sum.StrSum("1\n2,3"),6)
         
 
 if __name__ == '__main__':
