c7e2781f62289a03c14927fce40c1b18a75b4f24
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-08 14:42:03 +0530

    Formatting code

--- test_String_sum.py
+++ test_String_sum.py
@@ -1,27 +1,35 @@
 import unittest
 import String_sum
+
+
 class TestString_sum(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(String_sum.StrSum(""),0)
+        self.assertEqual(String_sum.StrSum(""), 0)
+
     def test_first(self):
-        self.assertEqual(String_sum.StrSum("1"),1)
-        self.assertEqual(String_sum.StrSum("1,2"),3)
+        self.assertEqual(String_sum.StrSum("1"), 1)
+        self.assertEqual(String_sum.StrSum("1,2"), 3)
+
     def test_Bigger_1000(self):
-        self.assertEqual(String_sum.StrSum("1,2000,1"),2)
-        self.assertEqual(String_sum.StrSum("1,2002,5,20"),26)
+        self.assertEqual(String_sum.StrSum("1,2000,1"), 2)
+        self.assertEqual(String_sum.StrSum("1,2002,5,20"), 26)
+
     def test_Negative(self):
-        stri="Negatives not allowed : -1 -3"
-        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***-3"),stri)
-        stri="Negatives not allowed : -1 "
-        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***3"),stri)
+        stri = "Negatives not allowed : -1 -3"
+        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***-3"), stri)
+        stri = "Negatives not allowed : -1 "
+        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***3"), stri)
+
     def test_alphabet(self):
-        self.assertEqual(String_sum.StrSum("1,2,a,c"),7)
-        self.assertEqual(String_sum.StrSum("1,2,a,c,d"),11) 
+        self.assertEqual(String_sum.StrSum("1,2,a,c"), 7)
+        self.assertEqual(String_sum.StrSum("1,2,a,c,d"), 11)
+
     def test_delimiter(self):
-        self.assertEqual(String_sum.StrSum("//;\n1;2,"),3) 
+        self.assertEqual(String_sum.StrSum("//;\n1;2,"), 3)
+
     def test_line_space(self):
-        self.assertEqual(String_sum.StrSum("1\n2,3"),6)
-        
+        self.assertEqual(String_sum.StrSum("1\n2,3"), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
