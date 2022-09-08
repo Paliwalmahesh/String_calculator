881e9f0e564c155e9c7d6796acbdff1ebc254e79
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-08 16:06:03 +0530

    Initial Testcase for multiplication

--- String_sum.py
+++ String_sum.py
@@ -1,74 +1,53 @@
-def LowerCharValue(ch):
+def lower_char_value(ch):
     if (ord('a') <= ord(ch) <= ord('z')):  # Adding the number corresponding to the alphabet
         return (ord(ch)-ord('a')+1)
 
-def StrSum(str):
-    if(str!="" and str[0]=="*"):
-        sum = 1
-        c = ''
-        msg = 'Negatives not allowed : '
-        neg = False
-        for i in str:
-            if i.isdigit():
-                c = c + i
-            else:
-                if c.isdigit(): # checking for digit
-                    
-                    if (neg == True):
-                        msg = msg+"-"+c+" "  # Adding number in string for exception
-                        neg = False
-                    elif (int(c) < 1000):
-                        sum = sum*int(c)  # Adding sum
-                    c = ''
-                if (ord(i) == ord('-')):  # for finding -ve number
-                    neg = True
-                sum *= LowerCharValue(i) if(LowerCharValue(i)) else  0
-                
-        else:        
-            if (c != '' and int(c) < 1001):
-                if c.isdigit():
-                    if (neg == True):
-                        msg = msg+"-"+c
-                        neg = False
-                else:
-                    sum = sum*int(c)
 
-        if (len(msg) > 24):
-            return msg  # exception for -ve number
+def opration_digit(s, li):
+    if (s != "" and li != []):
+        if (s[0] == "*"):
+            ans = 1
+            for i in li:
+                ans = ans*i
+            return ans
         else:
-            return sum
-    else:
-        sum = 0
-        c = ''
-        msg = 'Negatives not allowed : '
-        neg = False
-        for i in str:
-            if i.isdigit():
-                c = c + i
-            else:
-                if c.isdigit():  # checking for digit
-                    if (neg == True):
-                        msg = msg+"-"+c+" "  # Adding number in string for exception
-                        neg = False
-                    elif (int(c) < 1000):
-                        sum = sum+int(c)  # Adding sum
-                    c = ''
-                if (ord(i) == ord('-')):  # for finding -ve number
-                    neg = True
-                sum += LowerCharValue(i) if(LowerCharValue(i)) else  0
-                
-                
-        if (c != '' and int(c) < 1001):
-            if c.isdigit():
+            ans = 0
+            for i in li:
+                ans = ans+i
+            return ans
+
+
+def str_opration(s):
+    c = ''
+    li = []
+    msg = 'Negatives not allowed : '
+    neg = False
+    for i in s:
+        if i.isdigit():
+            c = c + i
+        else:
+            if c.isdigit():  # checking for digit
                 if (neg == True):
-                    msg = msg+"-"+c
+                    msg = msg+"-"+c+" "  # Adding number in string for exception
                     neg = False
-                else:
-                    sum = sum+int(c)
+                elif (int(c) < 1000):
+                    li.append(int(c))  # Adding sum
+                c = ''
+            if (ord(i) == ord('-')):  # for finding -ve number
+                neg = True
+            li.append(lower_char_value(i)) if (lower_char_value(i)) else 0
 
-        if (len(msg) > 24):
-            return msg  # exception for -ve number
-        else:
-            return sum
+    if (c != '' and int(c) < 1001):
+        if c.isdigit():
+            if (neg == True):
+                msg = msg+"-"+c
+                neg = False
+            else:
+                li.append(int(c))
 
-print(StrSum("*1\n3,3"))
+    if (len(msg) > 24):
+        return msg  # exception for -ve number
+    elif (opration_digit(s, li) != None):
+        return opration_digit(s, li)
+    else:
+        return 0
--- test_String_sum.py
+++ test_String_sum.py
@@ -4,34 +4,39 @@ import String_sum
 
 class TestString_sum(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(String_sum.StrSum(""), 0)
+        self.assertEqual(String_sum.str_opration(""), 0)
 
     def test_first(self):
-        self.assertEqual(String_sum.StrSum("1"), 1)
-        self.assertEqual(String_sum.StrSum("1,2"), 3)
+        self.assertEqual(String_sum.str_opration("1"), 1)
+        self.assertEqual(String_sum.str_opration("1,2"), 3)
 
     def test_Bigger_1000(self):
-        self.assertEqual(String_sum.StrSum("1,2000,1"), 2)
-        self.assertEqual(String_sum.StrSum("1,2002,5,20"), 26)
+        self.assertEqual(String_sum.str_opration("1,2000,1"), 2)
+        self.assertEqual(String_sum.str_opration("1,2002,5,20"), 26)
 
     def test_Negative(self):
         stri = "Negatives not allowed : -1 -3"
-        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***-3"), stri)
+        self.assertEqual(String_sum.str_opration("//[***]\n-1***2***-3"), stri)
         stri = "Negatives not allowed : -1 "
-        self.assertEqual(String_sum.StrSum("//[***]\n-1***2***3"), stri)
+        self.assertEqual(String_sum.str_opration("//[***]\n-1***2***3"), stri)
 
     def test_alphabet(self):
-        self.assertEqual(String_sum.StrSum("1,2,a,c"), 7)
-        self.assertEqual(String_sum.StrSum("1,2,a,c,d"), 11)
+        self.assertEqual(String_sum.str_opration("1,2,a,c"), 7)
+        self.assertEqual(String_sum.str_opration("1,2,a,c,d"), 11)
 
     def test_delimiter(self):
-        self.assertEqual(String_sum.StrSum("//;\n1;2,"), 3)
+        self.assertEqual(String_sum.str_opration("//;\n1;2,"), 3)
 
     def test_line_space(self):
-        self.assertEqual(String_sum.StrSum("1\n2,3"), 6)
+        self.assertEqual(String_sum.str_opration("1\n2,3"), 6)
+
+    def test_multiply_first(self):
+        self.assertEqual(String_sum.str_opration("*1"), 1)
+        self.assertEqual(String_sum.str_opration("*1,2"), 2)
 
     def test_multiply(self):
-        self.assertEqual(String_sum.StrSum("*1\n3,3"), 9)
+        self.assertEqual(String_sum.str_opration("*1\n3,3"), 9)
+        self.assertEqual(String_sum.str_opration("*1\n1000,3"), 3)
 
 
 if __name__ == '__main__':
